a639a789b5ad66f6d409d3e616d660e188a7790f
LuoYi99 <38639367+LuoYi99@users.noreply.github.com>
2018-05-08 20:54:50 +0800

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,18 @@
+/*
+题目：
+输入两个正整数a,b，输出a+b的值
+*/
+/*
+思路：
+
+*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("%d\n",a+b);
+	return 0;
+}
